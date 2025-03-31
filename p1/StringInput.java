630680b6c2c80f45864d178cba19dee3ecac7594
alam7865 <sabbajalam5@gmail.com>
2025-03-31 22:59:45 +0530

    Prac

---
+++ p1/StringInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StringInput {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine();
+        System.out.println("String :" + str);
+        System.out.println("DDDDD");
+    }
+}
