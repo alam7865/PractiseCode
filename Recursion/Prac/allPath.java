7c09a36018781fbc8a879a1c1b2b6c0f5649bbda
alam7865 <sabbajalam5@gmail.com>
2025-04-18 18:17:48 +0530

    Prac

--- Recursion/Prac/allPath.java
+++ Recursion/Prac/allPath.java
@@ -2,17 +2,22 @@ import java.util.*;
 
 public class allPath {
 
-    public static int solve(int n, int m, int i, int j, int[][] grid) {
+    public static int solve(int n, int m, int i, int j, int[][] grid, ArrayList<Integer> list) {
         if (i >= n || j >= m) {
             return 0;
         }
 
         if (i == n - 1 && j == m - 1) {
+            list.add(grid[i][j]);
+            System.out.println(list.toString());
             return 1;
         }
 
-        int right = solve(n, m, i, j + 1, grid);
-        int down = solve(n, m, i + 1, j, grid);
+        ArrayList<Integer> list2 = new ArrayList<>(list);
+        list2.add(grid[i][j]);
+
+        int right = solve(n, m, i, j + 1, grid, list2);
+        int down = solve(n, m, i + 1, j, grid, list2);
         return right + down;
     }
 
@@ -22,10 +27,11 @@ public class allPath {
                 { 4, 5, 6 }
         };
 
+        ArrayList<Integer> list = new ArrayList<>();
         int n = grid.length;
         int m = grid[0].length;
 
-        int ans = solve(n, m, 0, 0, grid);
+        int ans = solve(n, m, 0, 0, grid, list);
         System.out.println("Result: " + ans);
     }
 }
