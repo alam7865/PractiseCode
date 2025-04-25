320a17cd6ac1564d297a7f7e20d822f605104aa8
alam7865 <sabbajalam5@gmail.com>
2025-04-25 16:13:14 +0530

    Prac

--- jan13/countComSubarr.java
+++ jan13/countComSubarr.java
@@ -3,30 +3,52 @@ import java.util.*;
 public class countComSubarr {
     public static void main(String[] args) {
         int arr[] = { 1, 3, 1, 2, 2 };
-        int n = arr.length;
+        int n1 = arr.length;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n1; i++) {
+            set.add(arr[i]);
+        }
+
+        // System.out.println(set.size());
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
+        int i = 0;
+        int n = set.size();
+        int count = 0;
+        while (map.size() < n && i < n1) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            if (map.size() == n) {
+                count += (n1 - 1) - i + 1;
+            }
+            i++;
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(count);
+        int low = 0;
+        for (int j = i; j < n1; j++) {
+            // remove
+            while (map.get(arr[low]) > 1) {
+                map.put(arr[low], map.get(arr[low]) - 1);
+                if (map.get(arr[low]) == 0) {
+                    map.remove(arr[low]);
+                }
+                if (map.size() == n) {
+                    count++;
+                }
+                low++;
+            }
 
-        for (Map.Entry<Integer, Integer> set : map.entrySet()) {
-            int key = set.getKey();
-            int val = set.getValue();
-            if (val == 1) {
-                list.add(key);
+            // add
+            map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+            // calculate
+            if (map.size() == n) {
+                count++;
             }
         }
 
-        map.clear();
-
-        for (int i = 0; i < list.size(); i++) {
-            map.put(list.get(i), 1);
-        }
-
-        System.out.println(map.toString());
+        System.out.println(count);
     }
 }
