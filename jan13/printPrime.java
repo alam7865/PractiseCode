1316252f7a493f32e3615bea710e3fee2cc19eeb
alam7865 <sabbajalam5@gmail.com>
2025-05-10 23:43:04 +0530

    prac

--- jan13/printPrime.java
+++ jan13/printPrime.java
@@ -2,27 +2,34 @@ import java.util.*;
 
 public class printPrime {
     public static void main(String[] args) {
-        int n = 20;
-        boolean arr[] = new boolean[n + 1];
-        Arrays.fill(arr, true);
+        // int n = 20;
+        // boolean arr[] = new boolean[n + 1];
+        // Arrays.fill(arr, true);
 
-        for (int i = 2; i <= 20; i++) {
-            int pos = i;
-            if (arr[i] == true) {
-                while (pos + i <= n) {
-                    arr[pos + i] = false;
-                    pos += i;
-                }
-            }
-        }
+        // for (int i = 2; i <= 20; i++) {
+        // int pos = i;
+        // if (arr[i] == true) {
+        // while (pos + i <= n) {
+        // arr[pos + i] = false;
+        // pos += i;
+        // }
+        // }
+        // }
 
+        // ArrayList<Integer> list = new ArrayList<>();
+
+        // for (int i = 2; i <= 20; i++) {
+        // if (arr[i] == true) {
+        // list.add(i);
+        // }
+        // }
+
+        ////////////////////////////////////////////
         ArrayList<Integer> list = new ArrayList<>();
-
-        for (int i = 2; i <= 20; i++) {
-            if (arr[i] == true) {
-                list.add(i);
-            }
-        }
-
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.remove(list.size() - 1);
+        System.out.println(list.toString());
     }
 }
